5d19457af411135a2d09f8ebe2bd27e70f22e48a
captain-blue210 <captain.blue210@gmail.com>
2020-06-27 23:15:34 +0900

    add upload func

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 	"text/template"
+	"time"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -28,14 +33,47 @@ func login(w http.ResponseWriter, r *http.Request) {
 	} else {
 		r.ParseForm()
 		fmt.Println("username:", r.Form)
-		fmt.Println("username:", r.Form["username"])
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
 		fmt.Println("password:", r.Form["password"])
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
+	}
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		curtime := time.Now().Unix()
+		h := md5.New()
+
+		io.WriteString(h, strconv.FormatInt(curtime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.gtpl")
+		t.Execute(w, token)
+
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println("test", err)
+			return
+		}
+
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
 	}
 }
 
 func main() {
 	http.HandleFunc("/", sayhelloName)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/upload", upload)
 
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
